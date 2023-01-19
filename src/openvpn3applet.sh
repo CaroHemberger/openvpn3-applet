6d4e6aea6bbd2f949822b3bb2e6a3a04eaa1a040
Caroline Hemberger <caroline.hemberger@nesto-software.de>
2023-01-19 11:12:51 +0100

    use exported variable instead of function parameters

--- src/openvpn3applet.sh
+++ src/openvpn3applet.sh
@@ -33,6 +33,8 @@ mkfifo $PIPE
 # attach a file descriptor to the file
 exec 3<> $PIPE
 
+RUNNING_DIR=${BASH_SOURCE%/*}
+
 # add handler to manage process shutdown
 function on_exit() {
 	exec 3<> $PIPE
@@ -44,43 +46,42 @@ trap on_exit EXIT
 
 # add handler for tray icon left click
 function on_click() {
-    update_state $1
+    update_state
 }
 export -f on_click
 
 function disconnect() {
 	exec 3<> $PIPE
-	echo "icon:$1/icons/circle-lightblue.png" >&3
+	echo "icon:$RUNNING_DIR/icons/circle-lightblue.png" >&3
     sessionPath=$(openvpn3 sessions-list | grep Path | awk ' { print $2 } ')
     openvpn3 session-manage --disconnect --session-path $sessionPath
-    update_state $1
+    update_state
 }
 export -f disconnect
 
 function connect() {
 	exec 3<> $PIPE
-	echo "icon:$1/icons/circle-lightblue.png" >&3
+	echo "icon:$RUNNING_DIR/icons/circle-lightblue.png" >&3
 	configfile=$(yad --file)
     openvpn3 session-start --config $configfile
-    update_state $1
+    update_state
 }
 export -f connect
 
 function update_state() {
 	exec 3<> $PIPE
-	bashSource=$1
 	
 	output=$(openvpn3 sessions-list)
 	while IFS= read -r line; do
 		if [[ $line = "No sessions available" ]]
 		then
 			echo "no sessions"
-			echo "icon:$bashSource/icons/circle-red.png" >&3
+			echo "icon:$RUNNING_DIR/icons/circle-red.png" >&3
 			echo "tooltip:Not connected" >&3
 		elif [[ $line = *"Client connected" ]]
 		then
 			echo "sessions found!"
-			echo "icon:$bashSource/icons/circle-green.png" >&3
+			echo "icon:$RUNNING_DIR/icons/circle-green.png" >&3
 			echo "tooltip:Connected to VPN" >&3
 		fi
 	done <<< "$output"
@@ -89,19 +90,20 @@ function update_state() {
 
 export -f update_state
 export PIPE
+export RUNNING_DIR
 
 # create the notification icon
 yad --notification                  \
     --listen                        \
     --image="${BASH_SOURCE%/*}/icons/circle-red.png"  \
     --text="openvpn3-applet"        \
-    --command="bash -c 'on_click ${BASH_SOURCE%/*}'"   \
-    --menu="List sessions!${BASH_SOURCE%/*}/list-sessions.sh|Connect!bash -c 'connect ${BASH_SOURCE%/*}'|Disconnect!bash -c 'disconnect ${BASH_SOURCE%/*}'|Exit!exit 0" <&3 & notifpid=$!
+    --command="bash -c 'on_click'"   \
+    --menu="List sessions!$RUNNING_DIR/list-sessions.sh|Connect!bash -c 'connect'|Disconnect!bash -c 'disconnect'|Exit!exit 0" <&3 & notifpid=$!
     
 while true
 do 
 echo "notify: " $notifpid
-    update_state ${BASH_SOURCE%/*}
+    update_state
     sleep $sleepTime
 done
     
